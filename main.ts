4f1c6d0bab48e7e96b2feaf240e23f95b21fe439
Patrick Coxall <mr.coxall@mths.ca>
2022-06-09 08:57:03 -0400

    init

--- main.ts
+++ main.ts
@@ -1,1 +1,37 @@
-
+/**
+ * # Created by: Mr. Coxall
+ * 
+ * # Created on: Sep 2020
+ * 
+ * # This program shows declaring variables
+ */
+let mySprite = sprites.create(img`
+    . . . . . . . e c 7 . . . . . . 
+    . . . . e e e c 7 7 e e . . . . 
+    . . c e e e e c 7 e 2 2 e e . . 
+    . c e e e e e c 6 e e 2 2 2 e . 
+    . c e e e 2 e c c 2 4 5 4 2 e . 
+    c e e e 2 2 2 2 2 2 4 5 5 2 2 e 
+    c e e 2 2 2 2 2 2 2 2 4 4 2 2 e 
+    c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+    c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+    c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+    c e e 2 2 2 2 2 2 2 2 2 2 4 2 e 
+    . e e e 2 2 2 2 2 2 2 2 2 4 e . 
+    . 2 e e 2 2 2 2 2 2 2 2 4 2 e . 
+    . . 2 e e 2 2 2 2 2 4 4 2 e . . 
+    . . . 2 2 e e 4 4 4 2 e e . . . 
+    . . . . . 2 2 e e e e . . . . . 
+    `, SpriteKind.Player)
+let is_current = true
+let age = 32
+let area = 32.45
+let some_words = "Hello, World!"
+mySprite.sayText(is_current, 1000, true)
+pause(1000)
+mySprite.sayText(age, 1000, true)
+pause(1000)
+mySprite.sayText(area, 1000, true)
+pause(1000)
+mySprite.sayText(some_words, 1000, true)
+pause(1000)
